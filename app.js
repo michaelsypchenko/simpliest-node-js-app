823db70d51c89da499f3136edbe5ec764ee2fa1b
michaelsypchenko <uanauka01>
2019-08-01 13:28:15 +0300

    asdfas

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-const http = require("http");
+
+const https = require('https');
+const http = require('http');
+
 
 http.createServer(function(request, response){
 
-    response.end("Hello my world!");
+    https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
+        let data = '';
+
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            response.end("Hello my world! \n\n" + JSON.parse(data).explanation);
+            console.log(JSON.parse(data).explanation);
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+        response.end("Error: ");
+
+    });
 }).listen(3000);
