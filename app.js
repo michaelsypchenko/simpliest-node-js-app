427c6d26117ebc60728794c8412924a128b55591
michaelsypchenko <uanauka01>
2019-08-01 12:30:20 +0300

    asdfas

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const http = require("http");
-http.createServer(function(request,response){
 
-    response.end("Hello NodeJS!");
+http.createServer(function(request, response){
 
-}).listen(3000, "206.81.19.178",function(){
-    console.log("Сервер начал прослушивание запросов на порту 3000");
-});
+    response.end("Hello world!");
+}).listen(3000);
